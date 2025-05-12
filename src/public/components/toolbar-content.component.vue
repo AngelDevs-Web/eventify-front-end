0702ebf71897cd4d96f7d688c4da3ede6698dff8
Fabrizio <u201914181@upc.edu.pe>
2025-05-11 23:43:49 -0500

    fix(toolbar): fix organization of toolbar content

--- src/public/components/toolbar-content.component.vue
+++ src/public/components/toolbar-content.component.vue
@@ -1,28 +1,17 @@
 <script>
-import {Button as PvButton} from "primevue";
-
 export default {
-  name: "toolbar-content",
-  components: {PvButton},
-  data(){
-    return{
-      items:[
-        {label:"Home",to:"/home"},
-        {label:"Quotes",to:"quotes"},
-      ]
-    }
-  }
+  name: "toolbar-content"
 }
 </script>
 
 <template>
-  <pv-toolbar style="border-radius:0; background-color:#fff; border:none; padding:0">
+  <pv-toolbar class="flex flex-column" style="border-radius:0; background-color:#fff; border:none; padding:0;">
     <template #start>
       <div class="h-4rem"></div>
     </template>
     <template #center>
       <div class="flex flex-column">
-        <pv-button v-for="item in items"><router-link :to="item.to">{{ item.label }}</router-link></pv-button>
+        <a href="#">{{ $t('toolbar.home') }}</a>
         <a href="#">{{ $t('toolbar.profile') }}</a>
         <a href="#">{{ $t('toolbar.calendar') }}</a>
         <a href="#">{{ $t('toolbar.events') }}</a>
@@ -46,8 +35,7 @@ a{
   color: #000;
 }
 a:hover{
-  background-color: #3A506B;
-  color: #fff;
+  background-color: #EAEAEA;
   transition: 0.3s ease-in-out;
 }
 </style>
