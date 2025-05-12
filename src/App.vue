3a2b86707d176737eda98e93d15cbc085cc1ed0c
Fabrizio <u201914181@upc.edu.pe>
2025-05-11 23:47:44 -0500

    feat(main): integrate toolbar and header components with language switcher

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script>
+  import ToolbarContent from "./public/components/toolbar-content.component.vue";
+  import HeaderContent from "./public/components/header-content.component.vue";
+
   export default {
-    name: "app"
+    name: "app",
+    components: {HeaderContent, ToolbarContent}
   }
 </script>
 
 <template>
-
+  <header-content></header-content>
+  <div class="flex">
+    <toolbar-content></toolbar-content>
+  </div>
 </template>
 
 <style scoped>
