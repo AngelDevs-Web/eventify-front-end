4e302f654b85ddb6bda1a0f366dd6b7199619311
Fabrizio <u201914181@upc.edu.pe>
2025-05-11 21:58:37 -0500

    chore: add components to use in main source

--- src/main.js
+++ src/main.js
@@ -2,8 +2,37 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import i18n from "./i18n/index.js";
-
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Material from '@primeuix/themes/material';
+import {
+    Button,
+    Column,
+    ConfirmationService, ConfirmDialog,
+    DataTable,
+    Dialog,
+    DialogService,
+    Toast,
+    ToastService,
+    Toolbar
+} from "primevue";
 
 const app = createApp(App)
 
-app.use(i18n).mount('#app')
+app.use(PrimeVue,{theme:{preset:Material},ripple:true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .use(i18n)
+    .component('pv-dialog',Dialog)
+    .component('pv-button',Button)
+    .component('pv-data-table',DataTable)
+    .component('pv-column',Column)
+    .component('pv-toolbar',Toolbar)
+    .component('pv-toast',Toast)
+    .component('pv-confirm-dialog',ConfirmDialog)
+    .mount('#app')
+
+
+
